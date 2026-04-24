6e4e61c52cab5565c96dbbf4b677a4d6573ac713
Szemán Ábel <szeman.abel@gmail.com>
2026-04-24 14:19:14 +0200

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import type { Product } from "@/data/products"
+import ProductSheet from "./ProductSheet"
+
+const Navbar = () => {
+  return (
+    <header className="sticky top-0 z-10 border-b bg-background">
+      <div className="mx-auto flex h-16 max-w-5xl items-center justify-between px-4">
+        <span>📊 Webshop</span>
+        <span>Középen vagyok</span>
+        
+
+        <ProductSheet />
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
